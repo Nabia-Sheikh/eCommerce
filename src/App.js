a8151970c04d8d1c8f5a44b107fff457ef024981
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2022-07-26 20:18:34 +0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Footer from "./Component/Footer"
+import Header from "./Component/Header"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
